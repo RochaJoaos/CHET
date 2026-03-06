a47c77baf3da12ca57a1f27d380b8c9d630d2ddd
João Henrique <souza.joaohenrique.rocha@gmail.com>
2026-03-06 12:39:25 -0300

    Tela login

---
+++ chet-front/src/pages/Login/index.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+
+function Login() {
+    return (
+        <>
+        <section>
+            <h1>Entrar</h1>
+            <div className='inpbox'>
+                <label htmlFor="name">Email</label>
+                <input className="input" type="email" name="email" id="input-email"/>
+            </div>
+            <div className="inpbox">
+                <label for="name">Senha</label>
+                <input className="input" type="password" name="password" id="input-password"/>
+                <a href="..." target="_blank" rel="noopener noreferrer">Esqueceu sua senha?</a>
+                <input type="button" value="Continuar" className="btn"/>
+                <a href="..." target="_self" rel="noopener noreferrer">Crie sua conta</a>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Login
