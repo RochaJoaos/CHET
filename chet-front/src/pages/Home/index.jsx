3ceac970381f39df094bc6298fe5a339fac9911e
João Henrique <souza.joaohenrique.rocha@gmail.com>
2026-03-06 11:36:06 -0300

    implementação React

---
+++ chet-front/src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+
+function App() {
+  return (
+    <>
+      <main>
+        <section id='welcome'>
+          <div className='box'>
+                <h1>Bem-vindo ao CHET</h1>
+                <input type="button" value="Entrar" className="log"/>
+                <input type="button" value="Cadastrar" className="log"/>          
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
